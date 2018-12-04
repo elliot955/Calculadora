5f8497ad1be3a8bcc9af23f8b19db7fb34d565a0
elliot955 <ellliot955@hotmail.com>
2018-12-03 20:37:09 -0600

    actualizacion

--- calculadora.js
+++ calculadora.js
@@ -7,7 +7,26 @@ function borrar(){
 resultado.value = "";
 }
 function igual(){
+	if (resultado.value == '0000') {open('secr.html')}
 	resultados = document.getElementById('resultado').value;
 	resultado1 = eval(resultados);
 	resultado.value=resultado1;	
+}
+var con;
+var aux = 0;
+function guardar(){
+	con = document.getElementById("cont").value;
+	localStorage.contenido = con;
+	localStorage.aux = 1;
+	aux = "1";
+}
+function importar() {
+	if (localStorage.aux == 1) {
+		document.getElementById("cont").value = localStorage.contenido;
+	}
+}
+function test() {
+	if(localStorage.aux == undefined || localStorage.aux == 0){
+		document.getElementById("cont").value = "";
+	}
 }
